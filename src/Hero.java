a101469a2bd91a39065341349206cc6a5ce4a0c6
tatsuya828828 <kkjy011894@gmail.com>
2020-06-26 12:02:05 +0900

    [finish]13-3

--- src/Hero.java
+++ src/Hero.java
@@ -1,19 +1,38 @@
 
 public class Hero extends Character {
-	int hp = 100;
-	String name;
+	// このクラス内でのみ変更を許可、しかしこのクラスのメソッド経由なら変更可能
+	private int hp;
+	private String name;
 	Sword Sword;
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		if (name == null) {
+			throw new IllegalArgumentException("名前がnullである、処理を中断");
+		}
+		if (name.length() <= 1) {
+			throw new IllegalArgumentException("名前が短すぎる、処理を中断");
+		}
+		if (name.length() >= 8) {
+			throw new IllegalArgumentException("名前が長すぎる、処理を中断");
+		}
+		this.name = name;
+	}
 
 	public void bye() {
 		System.out.println("勇者は別れを告げた");
 	}
 
-	public void die() {
+	// このクラス内のみ呼び出し可能にする
+	private void die() {
 		System.out.println(this.name+"は死んでしまった！");
 		System.out.println("GAMEOVERです。");
 	}
 
-	public void sleep() {
+	void sleep() {
 		this.hp = 100;
 		System.out.println(this.name+"は眠って回復した");
 	}
